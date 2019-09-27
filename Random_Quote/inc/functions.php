8204aceb4ead55c0bc5fb64b0048e73ccb5d3a6a
T-Harry <53882274+T-Harry@users.noreply.github.com>
2019-09-27 13:40:47 -0500

    Add files via upload

---
+++ Random_Quote/inc/functions.php
@@ -0,0 +1,78 @@
+<?php
+// PHP - Random Quote Generator
+
+// Create the Multidimensional array of quote elements and name it quotes
+// Each inner array element should be an associative array
+
+$quotes = [
+   ['quote' => 'Ruby is rubbish! PHP is phpantastic!',
+   'source' => 'Nikita Popov',
+     'type' => 'Tech'],
+   ['quote' => 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+   'source' => 'Martin Fowler',
+     'type' => 'Tech'],
+   ['quote' => 'The way to get started is to quit talking and begin doing.',
+   'source' => 'Walt Disney',
+     'type' => 'Motivational'],
+   ['quote' => 'If you set your goals ridiculously high and it\'s a failure, you will fail above everyone else\'s success.',
+   'source' => 'James Cameron',
+     'type' => Null],
+   ['quote' => 'Tell me and I forget. Teach me and I remember. Involve me and I learn.',
+   'source' => 'Benjamin Franklin',
+     'type' => 'Motivational'],
+   ['quote' => 'Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma -- which is living with the results of other people\'s thinking.',
+   'source' => 'Steve Jobs',
+     'type' => 'Tech'],
+   ['quote' => 'The future belongs to those who believe in the beauty of their dreams.',
+   'source' => 'Eleanor Roosevelt',
+     'type' => 'Motivational'],
+   ['quote' => 'If we knew what it was we were doing, it would not be called research, would it?',
+   'source' => 'Albert Einstein',
+     'type' => Null],
+   ['quote' => "Student: Dr. Einstein, Aren't these the same questions as last year's physics final exam?<br>\n Dr. Einstein: Yes; But this year the answers are different.",
+   'source' => 'Albert Einstein',
+     'type' => Null],
+];
+
+function getRandomQuote($array) {
+  //Generates a random number based on length of array passed and returns that number
+  return $array[rand(0,count($array))];
+}
+
+function printQuote() {
+  //Bring $arrays into scope
+  global $quotes;
+  global $background_colors;
+  $quote = getRandomQuote($quotes);
+  $string = "";
+  //Assign and concatenate the string back to the $string variable
+  $string .= "<p class='quote'>" . $quote['quote'] . "</p>";
+  $string .= "<p class='source'>" . $quote['source'] . "</p>";
+  echo $string;
+  if ($quote['type'] == 'Tech') {
+    //Generates a random number to be used for the index value of the array
+    $background = $background_colors[0][rand(0,count($background_colors))];
+    echo '<body style="background-color:' . $background . '">';
+  } elseif ($quote['type'] == 'Motivational') {
+    $background = $background_colors[1][rand(0,count($background_colors))];
+    echo '<body style="background-color:' . $background . '">';
+  } else {
+    $background = $background_colors[2][rand(0,count($background_colors))];
+    echo '<body style="background-color:' . $background . '">';
+  }
+}
+
+$background_colors = array(
+  //Blues : Type - Technology
+  array('#4dc3ff','#0099e6','#4dc3ff','#33adff'),
+  //Greens : Type - Motivational
+  array('#33cc00','#39e600','#47d147','#70db70'),
+  //Purple : No Type
+  array('#c653c6','#bf40bf','#cc66cc','#ac39ac'),
+);
+
+
+
+
+
+?>
