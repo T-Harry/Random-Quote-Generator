c3c6a0bb9bced0b0193b855143d30810e73cc146
T-Harry <53882274+T-Harry@users.noreply.github.com>
2019-09-27 13:52:28 -0500

    Add files via upload

--- Random_Quote/inc/functions.php
+++ Random_Quote/inc/functions.php
@@ -11,18 +11,15 @@ $quotes = [
    ['quote' => 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
    'source' => 'Martin Fowler',
      'type' => 'Tech'],
-   ['quote' => 'The way to get started is to quit talking and begin doing.',
-   'source' => 'Walt Disney',
-     'type' => 'Motivational'],
-   ['quote' => 'If you set your goals ridiculously high and it\'s a failure, you will fail above everyone else\'s success.',
-   'source' => 'James Cameron',
-     'type' => Null],
-   ['quote' => 'Tell me and I forget. Teach me and I remember. Involve me and I learn.',
-   'source' => 'Benjamin Franklin',
-     'type' => 'Motivational'],
    ['quote' => 'Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma -- which is living with the results of other people\'s thinking.',
    'source' => 'Steve Jobs',
      'type' => 'Tech'],
+   ['quote' => 'The way to get started is to quit talking and begin doing.',
+   'source' => 'Walt Disney',
+     'type' => 'Motivational'],
+   ['quote' => 'Tell me and I forget. Teach me and I remember. Involve me and I learn.',
+   'source' => 'Benjamin Franklin',
+     'type' => 'Motivational'],
    ['quote' => 'The future belongs to those who believe in the beauty of their dreams.',
    'source' => 'Eleanor Roosevelt',
      'type' => 'Motivational'],
@@ -32,11 +29,15 @@ $quotes = [
    ['quote' => "Student: Dr. Einstein, Aren't these the same questions as last year's physics final exam?<br>\n Dr. Einstein: Yes; But this year the answers are different.",
    'source' => 'Albert Einstein',
      'type' => Null],
+   ['quote' => 'If you set your goals ridiculously high and it\'s a failure, you will fail above everyone else\'s success.',
+   'source' => 'James Cameron',
+     'type' => Null],
 ];
 
 function getRandomQuote($array) {
   //Generates a random number based on length of array passed and returns that number
-  return $array[rand(0,count($array))];
+  //Count function counts like a human - it starts at 1. -1 prevents showing a blank quote
+  return $array[rand(0,count($array) - 1)];
 }
 
 function printQuote() {
@@ -67,7 +68,7 @@ $background_colors = array(
   array('#4dc3ff','#0099e6','#4dc3ff','#33adff'),
   //Greens : Type - Motivational
   array('#33cc00','#39e600','#47d147','#70db70'),
-  //Purple : No Type
+  //Purples : No Type
   array('#c653c6','#bf40bf','#cc66cc','#ac39ac'),
 );
 
