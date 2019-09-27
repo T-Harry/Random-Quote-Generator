5b51ec075e98271ee8f92ee917968ea0d673efab
T-Harry <53882274+T-Harry@users.noreply.github.com>
2019-09-27 16:27:06 -0500

    Add files via upload
    Separated arrays from functions.php & re-factored code

---
+++ Random_Quote/inc/arrays.php
@@ -0,0 +1,74 @@
+<?php
+
+$quotes = [
+   ['quote' => 'Ruby is rubbish! PHP is phpantastic!',
+   'source' => 'Nikita Popov',
+     'type' => 'Tech',
+ 'citation' => Null,
+     'year' => Null],
+
+   ['quote' => 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+   'source' => 'Martin Fowler',
+     'type' => 'Tech',
+     'year' => Null,
+ 'citation' => Null],
+
+   ['quote' => 'Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma -- which is living with the results of other people\'s thinking.',
+   'source' => 'Steve Jobs',
+     'type' => 'Tech',
+ 'citation' => Null,
+     'year' => Null],
+
+   ['quote' => 'The way to get started is to quit talking and begin doing.',
+   'source' => 'Walt Disney',
+     'type' => 'Motivational',
+ 'citation' => Null,
+     'year' => Null],
+
+   ['quote' => 'Tell me and I forget. Teach me and I remember. Involve me and I learn.',
+   'source' => 'Xunzi (Xun Kuang)',
+     'type' => 'Motivational',
+ 'citation' => Null,
+     'year' => Null],
+
+   ['quote' => 'The future belongs to those who believe in the beauty of their dreams.',
+   'source' => 'Eleanor Roosevelt',
+     'type' => 'Motivational',
+ 'citation' => Null,
+     'year' => Null],
+
+   ['quote' => 'If we knew what it was we were doing, it would not be called research, would it?',
+   'source' => 'Albert Einstein',
+     'type' => Null,
+ 'citation' => Null,
+     'year' => Null],
+
+   ['quote' => "Student: Dr. Einstein, Aren't these the same questions as last year's physics final exam?<br>\n Dr. Einstein: Yes; But this year the answers are different.",
+   'source' => 'Albert Einstein',
+     'type' => Null,
+ 'citation' => Null,
+     'year' => Null],
+
+   ['quote' => 'If you set your goals ridiculously high and it\'s a failure, you will fail above everyone else\'s success.',
+   'source' => 'James Cameron',
+     'type' => Null,
+ 'citation' => Null,
+     'year' => Null],
+
+   ['quote' => 'If we\'re going to be damned, let\'s be damned for what we really are.',
+   'source' => 'Jean-Luc',
+     'type' => Null,
+ 'citation' => '"Startrek: The Next Generation" Encounter at Farpoint',
+     'year' => '1987'],
+];
+
+$background_colors = array(
+  //Blues : Type - Technology
+  array('#4dc3ff','#0099e6','#4dc3ff','#33adff'),
+  //Greens : Type - Motivational
+  array('#33cc00','#39e600','#47d147','#70db70'),
+  //Purples : No Type
+  array('#c653c6','#bf40bf','#cc66cc','#ac39ac'),
+);
+
+?>
--- Random_Quote/inc/functions.php
+++ Random_Quote/inc/functions.php
@@ -1,48 +1,7 @@
 <?php
 // PHP - Random Quote Generator
 
-$quotes = [
-   ['quote' => 'Ruby is rubbish! PHP is phpantastic!',
-   'source' => 'Nikita Popov',
-     'type' => 'Tech'],
-   ['quote' => 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-   'source' => 'Martin Fowler',
-     'type' => 'Tech'],
-   ['quote' => 'Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma -- which is living with the results of other people\'s thinking.',
-   'source' => 'Steve Jobs',
-     'type' => 'Tech'],
-   ['quote' => 'The way to get started is to quit talking and begin doing.',
-   'source' => 'Walt Disney',
-     'type' => 'Motivational'],
-   ['quote' => 'Tell me and I forget. Teach me and I remember. Involve me and I learn.',
-   'source' => 'Benjamin Franklin',
-     'type' => 'Motivational'],
-   ['quote' => 'The future belongs to those who believe in the beauty of their dreams.',
-   'source' => 'Eleanor Roosevelt',
-     'type' => 'Motivational'],
-   ['quote' => 'If we knew what it was we were doing, it would not be called research, would it?',
-   'source' => 'Albert Einstein',
-     'type' => Null],
-   ['quote' => "Student: Dr. Einstein, Aren't these the same questions as last year's physics final exam?<br>\n Dr. Einstein: Yes; But this year the answers are different.",
-   'source' => 'Albert Einstein',
-     'type' => Null],
-   ['quote' => 'If you set your goals ridiculously high and it\'s a failure, you will fail above everyone else\'s success.',
-   'source' => 'James Cameron',
-     'type' => Null],
-   ['quote' => 'If we\'re going to be damned, let\'s be damned for what we really are.',
-   'source' => 'Jean-Luc',
-     'type' => Null,
- 'citation' => '"Startrek: The Next Generation" Encounter at Farpoint'],
-];
-
-$background_colors = array(
-  //Blues : Type - Technology
-  array('#4dc3ff','#0099e6','#4dc3ff','#33adff'),
-  //Greens : Type - Motivational
-  array('#33cc00','#39e600','#47d147','#70db70'),
-  //Purples : No Type
-  array('#c653c6','#bf40bf','#cc66cc','#ac39ac'),
-);
+include 'inc/arrays.php';
 
 function getRandomQuote($array) {
   //Generates a random number based on length of array passed and returns that number
@@ -56,16 +15,23 @@ function printQuote() {
   global $background_colors;
   $quote = getRandomQuote($quotes);
   $string = "";
+
   //Assign and concatenate the string back to the $string variable
   $string .= "<p class='quote'>" . $quote['quote'] . "</p>";
-  $string .= "<p class='source'>" . $quote['source'] . "</p>";
+
   //Adds citaton if one exists within the array
-  if ($quote['citation'] !== Null) {
-    $string .= "<p class='citation'>" . $quote['citation'] . "</p>";
+  if ($quote['citation'] !== Null && $quote['year'] !== Null) {
+    $string .= "<p class='source'>" . $quote['source'] . "<span class='citation'>" . $quote['citation'] . "</span>" . "<span class='year'>" . $quote['year'] . "</span>" . "</p>";
   }
   //Adds year if one exists within the array
   elseif ($quote['year'] !== Null) {
-    $string .= "<p class='year'>" . $quote['year'] . "</p>";
+    $string .= "<p class='source'>" . $quote['source'] . "<span class='year'>" . $quote['year'] . "</span>" . "</p>";
+  }
+  elseif ($quote['citation'] !== Null) {
+    $string .= "<p class='source'>" . $quote['source'] . "<span class='citation'>" . $quote['citation'] . "</span>" . "</p>";
+  }
+  else {
+    $string .= "<p class='source'>" . $quote['source'] . "</p>";
   }
   echo $string;
 
@@ -74,10 +40,12 @@ function printQuote() {
     //Generates a random number to be used for the index value of the array
     $background = $background_colors[0][rand(0,count($background_colors))];
     echo '<body style="background-color:' . $background . '">';
-  } elseif ($quote['type'] == 'Motivational') {
+  }
+  elseif ($quote['type'] == 'Motivational') {
     $background = $background_colors[1][rand(0,count($background_colors))];
     echo '<body style="background-color:' . $background . '">';
-  } else {
+  }
+  else {
     $background = $background_colors[2][rand(0,count($background_colors))];
     echo '<body style="background-color:' . $background . '">';
   }
